b5bb18c72f10098747efd8355793694b1a2ef2f3
Tan05 <tan.tantham.tan@gmail.com>
2024-01-16 00:44:11 +0700

    Rendering map

---
+++ main.py
@@ -0,0 +1,3 @@
+from renderMap import RenderMaps
+
+print(RenderMaps().render_current_map())
---
+++ renderMap.py
@@ -0,0 +1,15 @@
+import maps
+
+class RenderMaps:
+    def __init__(self):
+        self.current_map = maps.current_map
+        self.symbolMeanings = [("0", " "), ("1", "X"),("2", "\n"),("3", "-")]
+        self.map = ""
+
+    def render_current_map(self):
+        for i in self.current_map:
+            for v in self.symbolMeanings:
+                if i == v[0]:
+                    self.map += v[1]
+
+        return self.map
