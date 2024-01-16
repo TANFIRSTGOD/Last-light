511c2222a3f7253e351c9562717fa79544905aa3
tan05 <tan.tantham.tan@gmail.com>
2024-01-17 00:07:03 +0700

    setup map rendering

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from renderMap import RenderMaps
+import time
+import os
+import platform
 
-print(RenderMaps().render_current_map())
+inGame = True
+
+while inGame:
+    print(RenderMaps().render_current_map())
+    if input(">>") == "quit()":
+        inGame = False
+    else:
+        operatingSys = platform.system()
+        match operatingSys:
+            case "Windows":
+                print("Windows")
